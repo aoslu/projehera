07fd2293531ba242cb6d9b606f597567a466c27b
Akın Oslu <aknoslu3541@gmail.com>
2021-07-27 03:26:14 +0300

    revize

--- projehera/settings.py
+++ projehera/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY=env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','127.0.0.1:8000','localhost','localhost:8000','18.157.178.157 ','18.157.178.157:8000']
+ALLOWED_HOSTS = ['127.0.0.1','127.0.0.1:8000','localhost','localhost:8000','18.157.178.157','18.157.178.157:8000']
 
 # Application definition
 
@@ -77,11 +77,15 @@ WSGI_APPLICATION = 'projehera.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME': env('DB_NAME'),
+      'USER': env('DB_USER'),
+      'PASSWORD': env('DB_PASSWORD'),
+      'HOST': 'localhost',
+      'PORT': '5432',
+   }
 }
 
 
